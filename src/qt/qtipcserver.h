200469205c6d8378d13897efe27d07242098276d
ulandort <kush@hotmail.co.id>
2017-06-07 08:53:38 -0400

    kingncoin
    Source of Kingn

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define KingNCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "KingNCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
